a4d87bd0397ac216201a3ceac62ece0c0d622ac2
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-01-26 21:46:39 +0100

    Adding random flickr picture; moving getRandomAlbum into separate function

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+var getRandomAlbum = function() {
+    var selfData = {title: "TestTitle"}
+    $.getJSON("http://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0&callback=?",
+	      {
+		  async: false
+	      },
+	      function(data) {
+		  selfData.band = data.query.random[0].title;
+	      }
+    );
+    $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?",
+	      {
+		  async: false
+	      },
+	      function(data) {
+		  selfData.cover = data.items[5].media.m;
+	      }
+    );
+    return selfData;
+}
+
 var Album = React.createClass({
     render: function() {
 	return (
@@ -11,23 +32,21 @@ var Album = React.createClass({
 
 var AlbumList = React.createClass({
     fetchAlbum: function() {
-	var selfData = {title: "TestTitle"}
-	$.ajax({
-	    url: "http://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0",
-	    dataType: "jsonp",
-	    crossDomain: true,
-	    success: function(data) {
-		selfData.band = data.query.random[0].title
-	    },
-	});
+	return getRandomAlbum();
     },
     getInitialState: function() {
 	var firstAlbum = this.fetchAlbum();
-	console.log("Calling fetch album!")
+	console.log("Calling fetch album!");
+	console.log(firstAlbum)
+	    
 	return { data: [firstAlbum] };
     },
     render: function() {
+	console.log("state");
+	console.log(this.state.data);
 	var albumsNodes = this.state.data.map(function (album) {
+	    console.log("Album:");
+	    console.log(album);
 	    return (
 		<Album
 		imageSRC="{album.cover}"
