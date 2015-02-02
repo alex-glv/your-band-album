f2de6b4a7d33e8f8fb7a086629907a5295f79ab8
Aleksandr Guljajev <alexandr.gulyayev@gmail.com>
2015-02-02 21:28:12 +0100

    Moved album fetch to album component; refactoring; cleanup; added loading state

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var jsonAjaxRequest = function jsonAjaxRequest(reqUrl, successFunction, argJsonpname) {
     var jsonpName = argJsonpname || "callback";
     $.ajax({
@@ -13,7 +11,6 @@ var getRandomWikiTitle = function(onDone) {
     jsonAjaxRequest("http://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=1&rnnamespace=0"
       , function(data) {
 	  var randomTitle = data.query.random[0].title;
-	  console.log("Title: " + randomTitle);
 	  onDone(randomTitle);
       });
 };
@@ -22,7 +19,6 @@ var getRandomFlickrImage = function(onDone) {
     jsonAjaxRequest("http://api.flickr.com/services/feeds/photos_public.gne?format=json"
 		    , function(data) {
 			var randomImage = data.items[5].media.m;
-			console.log("Rand title cover: " + randomImage);
 			onDone(randomImage);
 		    }
 		    , "jsoncallback"
@@ -41,7 +37,6 @@ var getRandomWikiQuote = function(onDone) {
 		var quotes = $("li",parsedText);
 		var randomElement = Math.round(Math.random() * quotes.length);
 		var randomQuote = $(quotes[randomElement]).text().replace(/^(.*?)[\.,:;-].*/, '$1');
-		console.log("Random quote is fetched: " + randomQuote);
 		onDone(randomQuote);
 	   }
 	);
@@ -54,59 +49,84 @@ var getRandomWikiQuote = function(onDone) {
 
 
 var Album = React.createClass({
-    render: function() {
-	return (
-		<div className='album'>
-		<span className='band'>{this.props.band}</span>
-		<span className='cover'><img src={this.props.cover} /></span>
-		<p className='title'>{this.props.title}</p>
-	    </div>
-	);
+    getInitialState: function() {
+	return {
+	    loading: true,
+	    band: null,
+	    title: null,
+	    cover: null
+	};
+    }
+    , componentDidMount: function() {
+	this.fetch();
+    }
+    , fetch: function() {
+	getRandomWikiTitle(function (band) { 
+	    this.setState({band: band});
+	    this.isLoaded();
+	}.bind(this));
+	
+	getRandomWikiQuote(function (title) { 
+	    this.setState({title: title});
+	    this.isLoaded();
+	}.bind(this));
+
+	getRandomFlickrImage(function (cover) { 
+	    this.setState({cover: cover});
+	    this.isLoaded();
+	}.bind(this));
+    }
+    , isLoaded: function() {
+	if (this.state.title !== null && this.state.band !== null && this.state.cover !== null) {
+	    this.setState({ loading: false });
+	} else {
+	    this.setState({ loading: true });
+	}
+    }
+    , render: function() {
+	if (this.state.loading) {
+	    return (
+		    <div className='album'>
+		    <div className='loading'>Loading...</div>
+		    </div>
+	    );
+	} else {
+	    return (
+		    <div className='album'>
+		    <span className='band'>{this.state.band}</span>
+		    <span className='cover'><img src={this.state.cover} /></span>
+		    <p className='title'>{this.state.title}</p>
+		    </div>
+	    );
+	}
     }
 });
 
 var AlbumList = React.createClass({
-    render: function() {
-	var albums = this.props.children.map(function (album) {
-	    return ( <Album cover={album.cover} title={album.title} band={album.band} /> );
+    getInitialState: function() {
+	return { albums: [{}] }
+    }
+    , getMore: function() {
+	var albums = this.state.albums;
+	albums.push({});
+	this.setState({albums: albums});
+    }
+    , render: function() {
+	var albums = this.state.albums.map(function (album) {
+	    return <Album />;
 	});
 	
 	return (
 		<div>
-		<button onClick={this.props.fetchFunc}>More!</button>
+		<button onClick={this.getMore}>More!</button>
 		<div className="albumsList">{albums}</div>
 		</div>
 	);
     }
 });
 
-
-var albums = [];
-
-var pushAlbum = function(album) {
-    if (album.title !== undefined && album.band !== undefined && album.cover !== undefined) {
-	console.log("Pushing now:");
-	console.log(album);
-	albums.push(album);
-	console.log(albums);
-
-	React.render(
-		<AlbumList fetchFunc={fetchAlbum}>
-		{albums}
-		</AlbumList>
-		, document.body);
-    }
-    return album;
-};
-
-var fetchAlbum = function() {
-    var newAlbum = {};
-    getRandomWikiTitle(function (band) { newAlbum.band = band; pushAlbum(newAlbum); });
-    getRandomWikiQuote(function (title) { newAlbum.title = title; pushAlbum(newAlbum); });
-    getRandomFlickrImage(function (cover) { newAlbum.cover = cover; pushAlbum(newAlbum); });
-};
-
-
 $(document).ready(function () {
-    fetchAlbum();
+    React.render(
+	    <AlbumList/>
+	    , document.body);
 });
